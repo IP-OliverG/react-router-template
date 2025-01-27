ebcbc264b51f45fcc320d3463485fe513f57fb18
Oliver <Oliver@intelligentplugins.co.uk>
2025-01-27 16:34:06 +0000

    removed meta from home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+//import Banner from '~/components/banner'
+//import Cms from '~/components/cms'
+import Contact from '~/components/contact'
+import FAQs from '~/components/faqs'
+import Features from '~/components/features'
+import Footer from '~/components/footer'
+//import Hero from '~/components/hero'
+import Navbar from '~/components/navbar'
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function meta({}: Route.MetaArgs ) {
+    return [
+        { title: 'mycomplaints.ai' },
+    ]
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function IndexRoute() {
+    return (
+        <div className='dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,hsla(var(--primary)_/_30%),#ffffff00)]'>
+            <Navbar />
+            {/* <Hero /> */}
+            {/* <Cms /> */}
+            <Features />
+            {/* <Banner /> */}
+            <FAQs />
+            <Contact />
+            <Footer />
+        </div>
+        
+    )
 }
