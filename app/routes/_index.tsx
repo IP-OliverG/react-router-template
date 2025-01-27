96b4b7dbbe60f3078a0da312524daaefac33551e
Oliver <Oliver@intelligentplugins.co.uk>
2025-01-27 17:21:38 +0000

    added hero

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import type { Route } from "./+types/_index";
 import Navbar from "~/components/navbar";
 import Cms from "~/components/cms";
 import Footer from "~/components/footer";
+import Hero from "~/components/hero";
 
 export function meta({}: Route.MetaArgs ) {
     return [
@@ -13,7 +14,7 @@ export default function IndexRoute() {
     return (
         <div className='dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,hsla(var(--primary)_/_30%),#ffffff00)]'>
             <Navbar />
-            {/* <Hero /> */}
+            <Hero />
             <Cms />
             {/* <Features /> */}
             {/* <Banner /> */}
