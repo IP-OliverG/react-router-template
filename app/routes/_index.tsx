3a0d253bae9227fe44253fafc0edb2e3b535c7f9
Oliver <Oliver@intelligentplugins.co.uk>
2025-01-27 16:40:58 +0000

    added navbar to home.tsx and renamed to _index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { Route } from "./+types/home";
+import Navbar from "~/components/navbar";
+import type { Route } from "./+types/_index";
 
 export function meta({}: Route.MetaArgs ) {
     return [
@@ -9,7 +10,7 @@ export function meta({}: Route.MetaArgs ) {
 export default function IndexRoute() {
     return (
         <div className='dark:bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,hsla(var(--primary)_/_30%),#ffffff00)]'>
-          
+          <Navbar />
         </div>    
     )
 }
